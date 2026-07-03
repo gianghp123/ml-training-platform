29e2d84c1383d1ec2a4f01ed0f656f8409e07c26
gianghp123 <giangthpt123@gmail.com>
2026-07-03 15:22:27 +0700

    feat: user button and auth middleware for webweb

--- web/app/(dashboard)/layout.tsx
+++ web/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import { UserSideBar } from "@/components/layouts/UserSideBar";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Layout({ children, breadcrumb }: { children: React.ReactNode; breadcrumb: React.ReactNode }) {
   return (
@@ -11,6 +12,9 @@ export default function Layout({ children, breadcrumb }: { children: React.React
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="my-auto mr-2 h-4" />
           {breadcrumb}
+          <div className="ml-auto flex items-center">
+            <UserButton />
+          </div>
         </header>
         {children}
       </SidebarInset>
