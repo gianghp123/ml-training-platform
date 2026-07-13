7c0deb25cb0125b28ab032156ed79ab5f2c2a096
gianghp123 <giangthpt123@gmail.com>
2026-07-13 20:23:26 +0700

    chore: bootstrap @training-ml/pipeline-engine package

---
+++ packages/pipeline-engine/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['**/tests/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+};
